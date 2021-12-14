9882bbe877ee3b98cfd605593dbbe3df4a16c12d
Alexey.Pestov <alexpestov89@yandex.ru>
2021-12-14 17:21:33 +0300

    add system.js -2

---
+++ src/main/resources/js/system.js
@@ -0,0 +1,6 @@
+$(function () {
+    console.log("dsafadsf");
+    var newOption = document.createElement("option");
+    newOption.innerHTML = 'test'
+    document.querySelector('#system').append(newOption)
+})
