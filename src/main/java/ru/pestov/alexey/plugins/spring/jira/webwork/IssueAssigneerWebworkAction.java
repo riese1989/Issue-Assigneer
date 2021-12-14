a1cd04ea0473b1b772d143cfd51c01ae9b4a9f88
Alexey.Pestov <alexpestov89@yandex.ru>
2021-12-14 17:21:11 +0300

    add system.js

--- src/main/java/ru/pestov/alexey/plugins/spring/jira/webwork/IssueAssigneerWebworkAction.java
+++ src/main/java/ru/pestov/alexey/plugins/spring/jira/webwork/IssueAssigneerWebworkAction.java
@@ -1,6 +1,9 @@
 package ru.pestov.alexey.plugins.spring.jira.webwork;
 
+import com.atlassian.jira.component.ComponentAccessor;
+import com.atlassian.jira.issue.IssueManager;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
+import com.atlassian.plugin.webresource.WebResourceManager;
 import com.atlassian.sal.api.pluginsettings.PluginSettings;
 import com.atlassian.sal.api.pluginsettings.PluginSettingsFactory;
 import lombok.Data;
@@ -28,9 +31,11 @@ public class IssueAssigneerWebworkAction extends JiraWebActionSupport
     @Inject
     public IssueAssigneerWebworkAction(@ComponentImport PluginSettingsFactory pluginSettingsFactory) {
             this.pluginSettings = pluginSettingsFactory.createGlobalSettings();
-            this.step1 = pluginSettings.get(PLUGIN_STORAGE_KEY + "step1") == null ? "no value" : pluginSettings.get(PLUGIN_STORAGE_KEY + "step1").toString();
-            this.step2 = pluginSettings.get(PLUGIN_STORAGE_KEY + "step2") == null ? "no value" : pluginSettings.get(PLUGIN_STORAGE_KEY + "step2").toString();
-            this.step3 = pluginSettings.get(PLUGIN_STORAGE_KEY + "step3") == null ? "no value" : pluginSettings.get(PLUGIN_STORAGE_KEY + "step3").toString();
+            this.step1 = pluginSettings.get(PLUGIN_STORAGE_KEY + "step1") == null ? "" : pluginSettings.get(PLUGIN_STORAGE_KEY + "step1").toString();
+            this.step2 = pluginSettings.get(PLUGIN_STORAGE_KEY + "step2") == null ? "" : pluginSettings.get(PLUGIN_STORAGE_KEY + "step2").toString();
+            this.step3 = pluginSettings.get(PLUGIN_STORAGE_KEY + "step3") == null ? "" : pluginSettings.get(PLUGIN_STORAGE_KEY + "step3").toString();
+            this.typeChange = pluginSettings.get(PLUGIN_STORAGE_KEY + "typechange") == null ? "" : pluginSettings.get(PLUGIN_STORAGE_KEY + "typechange").toString();
+            this.system = pluginSettings.get(PLUGIN_STORAGE_KEY + "system") == null ? "" : pluginSettings.get(PLUGIN_STORAGE_KEY + "system").toString();
     }
 
     @Override
@@ -39,9 +44,18 @@ public class IssueAssigneerWebworkAction extends JiraWebActionSupport
         return SUCCESS; //returns SUCCESS
     }
     public String doSave() {
+        System.out.println("****************************");
+        System.out.println("System " + this.system);
+        System.out.println("type change " + this.typeChange);
+        System.out.println("step1 " + this.step1);
+        System.out.println("step2 " + this.step2);
+        System.out.println("step3 " + this.step3);
+        System.out.println("****************************");
         this.pluginSettings.put(PLUGIN_STORAGE_KEY + "step1", this.step1);
         this.pluginSettings.put(PLUGIN_STORAGE_KEY + "step2", this.step2);
         this.pluginSettings.put(PLUGIN_STORAGE_KEY + "step3", this.step3);
+        this.pluginSettings.put(PLUGIN_STORAGE_KEY + "typechange", this.typeChange);
+        this.pluginSettings.put(PLUGIN_STORAGE_KEY + "system", this.system);
         return SUCCESS;
     }
 }
