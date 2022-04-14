6deee9b9f52f1b524dccdb2405b1526f2b3f3016
alexey.pestov <alexpestov89@yandex.ru>
2022-04-14 18:18:32 +0300

    14.04

--- src/main/resources/js/get-active-users.js
+++ src/main/resources/js/get-active-users.js
@@ -3,18 +3,14 @@ $(function () {
     var jiraURL = currentURL.split("secure")[0]
     const jiraRestAddress = jiraURL + 'rest/cab/1.0/systems/'
     $.get(jiraRestAddress + 'getactiveusers', function (response) {
-        console.log(jiraRestAddress + 'getactiveusers')
         const arrayUsers = response.substring(1, response.length - 1).split(", ")
         const stages = ["#stage1", "#stage21", "#stage22", "#stage23", "#stage3", "#authorize", "#delivery"]
-        $.each(stages, function (idStage) {
-            $.each(arrayUsers, function (idUser)   {
-                var user = arrayUsers[idUser]
-                console.log(user)
-                console.log(stages[idStage])
+        $.each(stages, function (idStage, stage) {
+            $.each(arrayUsers, function (idUser, user)   {
                 var newOption = document.createElement("option")
                 newOption.innerHTML = user.split("=")[0]
                 newOption.value = user.split("=")[1]
-                document.querySelector(stages[idStage]).append(newOption)
+                document.querySelector(stage).append(newOption)
             })
         })
     });
--- src/main/resources/js/test.js
+++ src/main/resources/js/test.js
@@ -1,21 +1,19 @@
-$(function () {
-    var currentURL = window.location.protocol + "//" + window.location.host + window.location.pathname + window.location.search
-    var jiraURL = currentURL.split("secure")[0]
-    const jiraRestAddress = jiraURL + 'rest/cab/1.0/systems/'
-    $.get(jiraRestAddress + 'getactiveusers', function (response) {
-        console.log(jiraRestAddress + 'getactiveusers')
-        const arrayUsers = response.substring(1, response.length - 1).split(", ")
-        const stages = ["#stage1", "#stage21", "#stage22", "#stage23", "#stage3", "#authorize", "#delivery"]
-        $.each(stages, function (idStage) {
-            $.each(arrayUsers, function (idUser)   {
-                var user = arrayUsers[idUser]
-                console.log(user)
-                console.log(stages[idStage])
-                var newOption = document.createElement("option")
-                newOption.innerHTML = user.split("=")[0]
-                newOption.value = user.split("=")[1]
-                document.querySelector(stages[idStage]).append(newOption)
-            })
+$(document).ready(function() {
+    $('.checkboxes').change(function (){
+        $('#systemCab').empty()
+        $('#systemCab').append(new Option("Select", "0"))
+        var values = ""
+        $(':checkbox').each(function() {
+            if (this.checked)   {
+                values += this.value + ","
+            }
         })
-    });
+        var jiraURL = $(location).attr("href").split("secure")[0]
+        var jiraRestAddress = jiraURL + 'rest/cab/1.0/systems/'
+        if (values !== "")  {
+            console.log(jiraRestAddress + 'getlistsystems?valuefilter=' + values)
+            $.get(jiraRestAddress + 'getlistsystems?valuefilter=' + values, function (response) {
+            })
+        }
+    })
 })
