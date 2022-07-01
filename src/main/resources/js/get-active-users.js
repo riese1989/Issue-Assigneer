ec00d396f53ace8a79edaa350f198a7539a6ba4c
alexey.pestov <alexpestov89@yandex.ru>
2022-07-01 12:51:22 +0300

    correct hide&mark inactive users

--- src/main/resources/js/get-active-users.js
+++ src/main/resources/js/get-active-users.js
@@ -2,7 +2,7 @@ $(function () {
     var currentURL = window.location.protocol + "//" + window.location.host + window.location.pathname + window.location.search
     var jiraURL = currentURL.split("secure")[0]
     const jiraRestAddress = jiraURL + 'rest/cab/1.0/systems/'
-    $.get(jiraRestAddress + 'getactiveusers', function (response) {
+    $.get(jiraRestAddress + 'getusers', function (response) {
         const arrayUsers = response.substring(1, response.length - 1).split(", ")
         const stages = ["#stage1", "#stage21", "#stage22", "#stage23", "#stage3", "#authorize", "#delivery"]
         $.each(stages, function (idStage, stage) {
